7e24483fd256a7cbf2a5e0cb9685da3969f2c0a3
Matheus Castro <goca1377@gmail.com>
2025-04-04 08:11:51 -0300

    feat: check auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,16 @@
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
 import { SigninCard } from "@/modules/auth/components/signin-card";
 
-export default function LoginPage () {
+export default async function LoginPage () {
+  const session = await auth()
+  
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <SigninCard />
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth()
 
-  console.log({ session })
+  if (!session || !session.user) {
+    redirect("/login");
+  }
+  
   return (
     <div>
       <h1>Hello NextJS + Shadcn-UI</h1>
